0fc9a70cd06903d55ceddbb9ae8b5208966a6edf
Steven Miyakawa SAM <samprofessional@gmail.com>
2016-01-30 18:23:04 -0800

    MVP - Functional, but not styled and no settings page

--- tipcalc/ViewController.swift
+++ tipcalc/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buildField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let billAmount = (buildField.text! as NSString).doubleValue
+        let tip = billAmount * getTipPercentage()
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    func getTipPercentage() -> Double {
+        let tipPercentages = [0.15, 0.18, 0.2]
+        return tipPercentages[tipControl.selectedSegmentIndex]
+    }
 
 }
 
